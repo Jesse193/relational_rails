37be531ced1151670c0592299e29d972de9b72b6
Jesse Sorman <jessesorman@gmail.com>
2023-05-10 17:40:42 -0400

    add parent index user story and test

---
+++ spec/features/shops/show_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe "shops index page", type: :feature do
+  # For each shop table
+  # As a visitor
+  # When I visit '/shops'
+  # Then I see the name of each shop record in the system
+  it "can display all shop names" do
+    shop1 = Shop.create!(name: "North Pole Creamery", hours: 1100/2000, open_year_round: true)
+    shop2 = Shop.create!(name: "Ice Caps", hours: 1200/2200, open_year_round: false)
+    expect(page).to have_content(shop1.name)
+    expect(page).to have_content(shop2.name)
+  end
+end
