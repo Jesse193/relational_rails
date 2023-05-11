c2ac37caa01e2b3a5c60c389c012999b793a21ed
Jesse Sorman <jessesorman@gmail.com>
2023-05-11 17:57:46 -0400

    refactor shop show tests

--- spec/features/shops/show_spec.rb
+++ spec/features/shops/show_spec.rb
@@ -1,14 +1,21 @@
-# require "rails_helper"
-# RSpec.describe "shops shops attributes" do
+require "rails_helper"
+RSpec.describe "shops shops attributes", type: :feature do
 
-#   # As a visitor
-#   # When I visit '/shops/:id'
-#   # Then I see the parent with that id including the parent's attributes
-#   it "can display all shop id and attributes" do
-#     shop_1 = Shop.create!(name: "North Pole Creamery", hours: "11:00 - 20:00", open_year_round: true, established: 1980)
-#     shop_2 = Shop.create!(name: "Ice Caps", hours: "12:00 - 22:00", open_year_round: false, established: 1993)
-#     visit "/shops/#{shop_1.id}"
-#     visit "/shops/#{shop_2.id}"
-#     expect(page).to have_content(shop_1.hours)
-#   end
-# end
+  # As a visitor
+  # When I visit '/shops/:id'
+  # Then I see the parent with that id including the parent's attributes
+  it "can display all shop id and attributes" do
+    shop_1 = Shop.create!(name: "North Pole Creamery", hours: "11:00 - 20:00", open_year_round: true, established: 1980)
+    visit "/shops/#{shop_1.id}"
+    expect(page).to have_content(shop_1.hours)
+    expect(page).to have_content(shop_1.open_year_round)
+    expect(page).to have_content(shop_1.established)
+
+    shop_2 = Shop.create!(name: "Ice Caps", hours: "12:00 - 22:00", open_year_round: false, established: 1993)
+    visit "/shops/#{shop_2.id}"
+    expect(page).to have_content(shop_2.hours)
+    expect(page).to have_content(shop_2.open_year_round)
+    expect(page).to have_content(shop_2.established)
+
+  end
+end
