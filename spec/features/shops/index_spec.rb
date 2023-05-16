f3ddc025817e20191022aa903683161083674ad4
Jesse Sorman <jessesorman@gmail.com>
2023-05-15 21:29:24 -0400

    add test for user story 6

--- spec/features/shops/index_spec.rb
+++ spec/features/shops/index_spec.rb
@@ -12,4 +12,15 @@ RSpec.describe "shops index page" do
     expect(page).to have_content(shop_1.name)
     expect(page).to have_content(shop_2.name)
   end
+  # As a visitor
+  # When I visit the shop index,
+  # I see that records are ordered by most recently created first
+  # And next to each of the records I see when it was created
+  it "can put shops in order by creation" do
+    shop_1 = Shop.create!(name: "North Pole Creamery", hours: "11:00 - 2000", open_year_round: true, established: 1980)
+    shop_2 = Shop.create!(name: "Ice Caps", hours: "12:00 - 22:00", open_year_round: false, established: 1990)
+    visit "/shops"
+    expect(page).to have_content(shop_2.created_at)
+    expect(page).to have_content(shop_1.created_at)
+  end
 end
