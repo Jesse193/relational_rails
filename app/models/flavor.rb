600768a21f794f4f7b890f73f33bb7d7cfadab43
Jesse Sorman <jessesorman@gmail.com>
2023-05-16 16:37:49 -0400

    add model test for only flavors that are true

--- app/models/flavor.rb
+++ app/models/flavor.rb
@@ -1,3 +1,7 @@
 class Flavor < ApplicationRecord
   belongs_to :shop
+
+  def self.only_true
+    self.where(nut_free: true)
+  end
 end
--- spec/models/flavor_spec.rb
+++ spec/models/flavor_spec.rb
@@ -4,4 +4,15 @@ describe Flavor, type: :model do
   describe "relationships" do
     it {should belong_to(:shop)}
   end
+  # As a visitor
+  # When I visit the flavor index
+  # Then I only see records where the boolean column is `true`
+  describe "only true" do
+    it "only shows flavors that are true" do
+      shop = Shop.create!(name: "North Pole Creamery", hours: "11:00 - 2000", open_year_round: true, established: 1980)
+      flavor = shop.flavors.create!(flavor: "Chocolate", nut_free: true, calories: 600)
+      flavor2 = shop.flavors.create!(flavor: "Chocolate Almond", nut_free: false, calories: 600)
+      expect(Flavor.only_true).to eq([flavor])
+    end
+  end
 end
